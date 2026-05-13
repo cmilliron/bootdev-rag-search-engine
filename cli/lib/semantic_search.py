90440015b90c359dd0f6923de2822914f2ac7ca8
Cody Milliron <codymilliron.dev@gmail.com>
2026-05-13 16:24:44 -0400

    add chunking

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,6 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
-from lib.search_utils import EMBEDDINGS_CACHE_PATH, load_movies, CACHE_DIR
+from lib.search_utils import (
+    EMBEDDINGS_CACHE_PATH, 
+    load_movies, 
+    CACHE_DIR, 
+    DEFAULT_SEARCH_LIMIT, 
+    DEFAULT_CHUNK_SIZE
+    )
 
 class SemanticSearch:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
@@ -31,7 +37,8 @@ class SemanticSearch:
     
     def save_embedding(self):
         CACHE_DIR.mkdir(parents=True, exist_ok=True)
-        np.save(self.embeddings_path, self.embeddings)
+        if self.embeddings:
+            np.save(self.embeddings_path, self.embeddings)
 
 
     def load_or_create_embedding(self, documents):
@@ -44,6 +51,29 @@ class SemanticSearch:
             if len(self.embeddings) == len(self.documents):
                 return self.embeddings
         return self.build_embeddings(documents)
+    
+
+    def search(self, query: str, limit: int = DEFAULT_SEARCH_LIMIT):
+        if self.embeddings is None or self.embeddings.size == 0:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
+        if self.documents is None or len(self.documents) == 0:
+            raise ValueError("No documents loaded. Call `load_or_create_embeddings` first.")
+        query_embed = self.generate_embedding(query)
+        simularities = []
+        for i, e in enumerate(self.embeddings):
+            cos_sim = cosine_similarity(query_embed, e)
+            simularities.append((cos_sim, self.documents[i]))
+        formated_results = []
+        for score, movie in sorted(simularities, key=lambda x: x[0], reverse=True)[:limit]:
+            formated_results.append({
+                "score": score,
+                "title": movie["title"],
+                "description": movie['description'] 
+            })
+        return formated_results
+
+
+
     
 
 
@@ -73,3 +103,51 @@ def embed_query_text(query):
     print(f"Query: {query}")
     print(f"First 3 dimensions: {query_embedding[:3]}")
     print(f"Shape: {query_embedding.shape}")
+
+
+def cosine_similarity_search(query: str, limit: int = DEFAULT_SEARCH_LIMIT):
+    ss = SemanticSearch()
+    movies = load_movies()
+    ss.load_or_create_embedding(documents=movies)
+    results = ss.search(query, limit)
+    print(f"Query: {query}")
+    print(f"Top {len(results)} results:")
+    print()
+
+    for i, result in enumerate(results, 1):
+        print(f"{i}. {result['title']} (score: {result['score']})")
+        print(f"    {result["description"][:25]}...")
+
+
+def fixed_size_chunking(text: str, chunk_size: int = DEFAULT_CHUNK_SIZE):
+    words = text.split()
+    chunks = []
+    num_words = len(words)
+    i = 0
+    while i < num_words:
+        current_chunk = words[i: i + chunk_size]
+        chunks.append(current_chunk)
+        i += chunk_size
+    return chunks
+
+
+def chunck_command(text: str, size: int = DEFAULT_CHUNK_SIZE):
+    chunks = fixed_size_chunking(text, size)
+    print(f"Chunking {len(text)} characters")
+    for i, c in enumerate(chunks, 1):
+        print(f"{i}. {" ".join(c)}")
+    return chunks
+
+
+
+
+# Helper Functions
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,8 +2,11 @@
 from lib.semantic_search import (
     verify_model, 
     embed_text,
-    verify_embeddings
+    verify_embeddings,
+    cosine_similarity_search,
+    chunck_command
 )
+from lib.search_utils import DEFAULT_SEARCH_LIMIT, DEFAULT_CHUNK_SIZE
 
 import argparse
 
@@ -20,6 +23,14 @@ def main():
     embed_query_parser = subparsers.add_parser("embed_query", help="Search for query.")
     embed_query_parser.add_argument('query', type=str, help="Text to embed.")
 
+    search_parser = subparsers.add_parser("search", help="Search for query.")
+    search_parser.add_argument('query', type=str, help="Text to embed.")
+    search_parser.add_argument('-l', '--limit', type=int, default=DEFAULT_SEARCH_LIMIT, help="Search Limit")
+   
+    chunk_parser = subparsers.add_parser("chunk", help="Command to chunk text.")
+    chunk_parser.add_argument('text', type=str, help="Text to embed.")
+    chunk_parser.add_argument('-c', '--chunk-size', dest="chunk_size", type=int, default=DEFAULT_CHUNK_SIZE, help="Chunk size")
+
     args = parser.parse_args()
 
     match args.command:
@@ -34,6 +45,12 @@ def main():
 
         case "embed_query":
             embed_text(args.query)
+
+        case "search":
+            cosine_similarity_search(args.query, args.limit)
+
+        case "chunk":
+            chunck_command(args.text, args.chunk_size)
 
         case _:
             parser.print_help()
